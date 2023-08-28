07a059b1b75e99a22e0e0621ee978f1bbb9e1d6a
KevinPedroso <kevinpedroso@pop-os.localdomain>
2023-08-28 07:54:28 -0300

    Add files AppSuperGestao

---
+++ resources/views/site/contato.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+            .titulo{
+                display: flex;
+            flex-direction: row;
+            justify-content: center;
+            align-items: center;
+            }
+            
+            .list{
+            display: flex;
+            flex-direction: row;
+            width: 100vw;
+            height: 10vh;
+            justify-content: center;
+            gap: 10px;
+        }
+        </style>
+    </head>
+    <body>
+        <h1 class="titulo">Contato</h1>
+        <div class="list">
+            <a href="{{ route('site.inicio') }}">Página Inicial</a>
+            <a href="{{ route('site.contato') }}">Contato</a>
+            <a href="{{ route('site.sobre') }}">Sobre nós</a>
+        </div>
+    </body>
+</html>
