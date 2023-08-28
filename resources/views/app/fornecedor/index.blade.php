3b99bc908148bfb78ca5e4f28880caed94d68cb2
alveskevinn <kevin.alves13@hotmail.com>
2023-08-28 10:24:26 -0300

    save changes

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,5 @@
+<h3>Fornecedores</h3>
+{{-- Comentario para php --}}
+@php
+
+@endphp
--- routes/web.php
+++ routes/web.php
@@ -20,10 +20,13 @@ Route::get('/login', function (){return "Login";})->name('site.login');
 
 Route::prefix("/app")->group( function(){
 
-    Route::get('/clientes', function (){return "Clientes";})->name('app.produtos');
-    Route::get('/fornecedores', function (){return "Fornecedores";})->name('app.produtos');
+    Route::get('/clientes', function (){return "Clientes";})->name('app.clientes');
+    Route::get('/fornecedores', 'FornecedoresController@index')->name('app.fornecedores');
     Route::get('/produtos', function (){return "Produtos";})->name('app.produtos');
 });
-Route::fallback(function(){
-    echo 'Essa página não existe';
-    })->name('site.paginaNaoExiste');
+Route::get("/teste/{p1}/{p2}", "TesteController@teste")->name('teste');
+
+/*Route::fallback(function(){
+//     echo 'Essa página não existe';
+//     })->name('site.paginaNaoExiste');
+*/;
